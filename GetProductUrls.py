c74f669e7c31b561a2be65cd85a6cbf037ef414b
taidao1901 <19522148@gm.uit.edu.vn>
2022-07-27 16:15:37 +0700

    update

--- GetProductUrls.py
+++ GetProductUrls.py
@@ -7,10 +7,14 @@ from selenium.webdriver.chrome.service import Service
 from time import sleep
 
 def Dynamic_web(link):
+    '''
+        Lấy nội dung html của dynamic website
+            link: link của website
+    '''
+    # Get infor in a dynamic page
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get(link)
     last_height = driver.execute_script("return document.body.scrollHeight")
-    # Kiểm tra đã load hết trang hay chưa ?
     while True:
         # Scroll down to bottom
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -21,57 +25,114 @@ def Dynamic_web(link):
         if new_height == last_height:
             break
         last_height = new_height
+    
+    # Convert to html type
     sele = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
     return sele
 
 def Get_product_url(content,rootlink='', parent_tag='', child_tag=''):
+    '''
+        Lấy các url sản phẩm trên 1 trang.
+            content: Nội dung của trang
+            rootlink: Dùng cho các trang dùng địa chỉ tương đối ( Không truyển nếu trang dùng địa chỉ tuyệt đối)
+            parent_tag: Class của thẻ div gần nhất
+            child-tag: Class của thẻ a chứa link sản phẩm (Không truyền nếu không có tên class)
+    '''
+
+    # Convert content in page to beutifulsoup type
     soup = BeautifulSoup(content, 'html.parser')
+
+    # find parent block that a tag in it
     product_list = soup.find_all('div', class_= parent_tag)
     productlinks=[]
     for product in product_list:
-        #Lấy ra link của sản phẩm. Nếu có tồn tại class thẻ q thì tìm theo class của thẻ a
+        # if a tag have class name then find it
         if child_tag =='':
             productlinks.append(rootlink + product.find_all('a',href=True)[0]['href'])
         else: 
             productlinks.append(rootlink + product.find_all('a',class_=child_tag,href=True)[0]['href'])
+    # return link of products
     return productlinks
     
-def Normal_Website(links, rootlink='', parent_tag='', child_tag=''):
+def Normal_Website(links, rootlink='', parent_tag='', child_tag='', dynamic = False):
     '''
         Hàm lấy thông tin của các trang có số trang
-            link: Truyền link tổng quát của website. Ví dụ: https://laminapparel.com/shop/page/{number}/
+            links: Truyền danh sách link lấy sản phẩm. Ví dụ: ['https://hades.vn/collections/top?page={number}','https://hades.vn/collections/bottoms?page={number}']']
+            rootlink: Dùng cho các trang dùng địa chỉ tương đối ( Không truyển nếu trang dùng địa chỉ tuyệt đối)
             parent_tag: Class thẻ div bên ngoài thẻ a
-            child_tag: class thẻ a ()
+            child_tag: Class của thẻ a chứa link sản phẩm (Không truyền nếu không có tên class)
     '''
     product_links = []
+
+    # excute in each link in links
     for link in links:
         num= 1
         while True:
-            content = requests.get(link.format(number=num)).content
-            length_link= len(productlinks)
+            # get content in each page number per link
+            content=''
+            if dynamic == False:
+                content = requests.get(link.format(number=num)).content
+            else: 
+                content = Dynamic_web(link.format(number=num))
+
+            # Calculate new list length and compare with last list length
+            length_link= len(product_links)
             product_links = product_links + Get_product_url(content,rootlink, parent_tag, child_tag)
+
+            # if length is changed, increase page number
             if length_link< len(product_links):
                 num +=1
             else:
                 break
+    # return list product links in normal website
     return product_links
 def Scroll_Website(links, rootlink='', parent_tag='', child_tag=''):
     '''
         Hàm lấy link sản phẩm từ các trang dạng cuộn
-            link:
-            rootlink:
-            parent_tag:
-            child_tag:
+            links: Danh sách link lấy sản phẩm. Ví dụ: ['https://ssstutter.com/c/for-him','https://ssstutter.com/c/for-her']
+            rootlink: Dùng cho các trang dùng địa chỉ tương đối ( Không truyển nếu trang dùng địa chỉ tuyệt đối)
+            parent_tag: Class thẻ div bên ngoài thẻ a
+            child_tag: Class của thẻ a chứa link sản phẩm (Không truyền nếu không có tên class)
     '''
+    # excute in each link in links
     product_links = []
     for link in links:
         content = Dynamic_web(link)
         product_links = product_links + Get_product_url(content,rootlink, parent_tag, child_tag)
-        print(len(product_links))
     return product_links
+
+def get_product_urls(links, rootlink='', parent_tag='', child_tag='',webtype='normal',dynamic=False):
+    '''
+        Hàm chính lấy link các sản phẩm của một website
+            links: Danh sách link lấy sản phẩm.
+            rootlink: Dùng cho các trang dùng địa chỉ tương đối ( Không truyển nếu trang dùng địa chỉ tuyệt đối)
+            parent_tag: Class thẻ div bên ngoài thẻ a
+            child_tag: Class của thẻ a chứa link sản phẩm (Không truyền nếu không có tên class)
+            webtype: loại website, mặc định là normal ('normal': cho các website sử dụng số trang, 'scroll': cho các website dạng cuộn)
+            dynamic: Có phải dạng dynamic website hay không, mặc đinh là False(False: cho các website không phải dạng dynamic, True: ngược lại )
+    '''
+    if webtype== 'normal':
+        product_links = Normal_Website(links, rootlink, parent_tag, child_tag, dynamic)
+        return product_links
+    elif webtype == 'scroll':
+        product_links = Scroll_Website(links,rootlink,parent_tag,child_tag)
+        return product_links
+    else: 
+        print('Webtype không tồn tại')
+        return []
+    
+
+
 if __name__ == '__main__':
-    product_links= NỎ(links=['https://huongboutique.vn/shop-online?page={number}}'], rootlink='https://ssstutter.com', parent_tag='thumbnail', child_tag='')
+    links=['https://hades.vn/collections/all?page={number}']
+    rootlink='https://hades.vn'
+    parent_tag='product-img'
+    child_tag = 'image-resize'
+    webtype = 'normal'
+    dynamic= False
+    product_links= get_product_urls(links, rootlink=rootlink, parent_tag=parent_tag, child_tag=child_tag,webtype=webtype,dynamic=dynamic)
     [print(x) for x in product_links]
+    print(len(product_links))
 
 
 
