3aafe764d824b682bd3bf0883987907343f8532b
ThanhHienLe168 <107921439+ThanhHienLe168@users.noreply.github.com>
2022-08-02 10:05:44 +0700

    Add files via upload

---
+++ Create_Json_Info.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import json
+
+
+# In[2]:
+
+
+input_info = {
+    
+    "shop_name":"",
+    "stylebox_shop_id":"",
+    "shop_url":"",
+    "isdynamic":"",
+    "json_file_name":"",
+    "csv_file_name":"",
+
+    "product_links": {
+
+        "links": "",
+        "rootlink":"",
+        "parent_tag":"",
+        "child_tag":"",
+
+    },
+
+
+    "main_info": {
+        
+        
+        "product_name": {
+            "name_tag":"",
+            "name_class":"",
+        },
+
+        "original_price": {
+            "original_price_tag":"",
+            "original_price_class":"",
+            "is_woocommerce":"",
+            "woocommerce_value":"",
+        },
+
+        "imgs": {
+            "img_parent_tag":"",
+            "img_parent_class":"",
+            "img_child_tag":"",
+            "img_child_class":"",
+            "img_child_attribute":"",
+        }
+    },
+
+
+    "sub_info": {
+        "discounted_price":"",
+        "review":"",
+        "size": "",
+        "color": "",
+        "description_1":"",
+        "description_2":"",
+        "rating":"",
+
+    }
+    
+}
+
+
+json_object = json.dumps(input_info, indent=8)
+with open("config.json", "w") as outfile:
+    outfile.write(json_object)
+
+
+# In[3]:
+
+
+import pprint
+pprint.pprint(input_info)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
