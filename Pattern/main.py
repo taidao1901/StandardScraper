415bc7fbc87ba25be72d839dfff6b7b11bd27bf6
ThanhHienLe168 <107921439+ThanhHienLe168@users.noreply.github.com>
2022-08-10 10:21:26 +0700

    Update main.py
    Suất ra thông báo khi lấy link sản phẩm.

--- Pattern/main.py
+++ Pattern/main.py
@@ -1,6 +1,6 @@
 from Get_Product_Info_Test import Test_Data
 from GetProductUrls import get_product_urls
-from Get_Data_Fi import web_scraping
+from Get_Data_Fi_2 import web_scraping
 import csv
 import json
 import sys
@@ -29,12 +29,13 @@ if __name__ == '__main__':
                 exit()
         result_folder_name = config['stylebox_shop_id'] +'_'+config['shop_name']
         #Get link products
+        print('Bắt đầu lấy link sản phẩm ....')
         start = time.time()
         try: 
             product_links= get_product_urls(config)
             try:
                 if "debug_links" in sys.argv:
-                    print('Tổng số sản phẩm là:', len(product_links))
+                    print('Tổng số sản phẩm lấy được link là:', len(product_links))
                     print('5 link sản phẩm đầu tiên là: ')
                     for i in range(5):
                         print(product_links[i])
@@ -54,8 +55,8 @@ if __name__ == '__main__':
                 if os.path.exists(os.path.join(result_path, result_folder_name))==False:
                     os.mkdir(os.path.join(result_path, result_folder_name))
                 data = web_scraping(config, product_links, os.path.join(result_path, result_folder_name))
-            
-            print('Thời gian lấy link sản phẩm: ', time.time()-start)
+                print('Đã lấy link sản phẩm xong .')
+            #print('Thời gian lấy link sản phẩm: ', time.time()-start)
             exit()
         except Exception:
             print('Lỗi lấy thông tin sản phẩm')
